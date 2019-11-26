eafc2bdc8da0d040319eb5deb369b6b1a4df6ed9
Poonam <poonam@codeblock.co.in>
2019-11-26 23:23:32 +0530

    change for ip add

--- handlers/login.js
+++ handlers/login.js
@@ -157,7 +157,7 @@ class Login extends BaseHandler {
         try {
             console.log("in process 2");
             let body = event.body ? JSON.parse(event.body) : event;
-            console.log(body);
+            console.log(event);
 
             // io.sockets.on('connection', function (socket) {
             //     var address = socket.handshake.address;
