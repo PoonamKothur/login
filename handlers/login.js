0677d84197d76cdf64e5dccb923ea6a8ebeb207e
Poonam <poonam@codeblock.co.in>
2019-12-01 00:00:09 +0530

    role

--- handlers/login.js
+++ handlers/login.js
@@ -5,7 +5,6 @@ const Joi = require('joi');
 const AWS = require('aws-sdk');
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 const cognitoidentityserviceprovider = new AWS.CognitoIdentityServiceProvider();
-//const io = require('socket.io').listen(server);
 
 class Login extends BaseHandler {
     //this is main function
@@ -27,7 +26,7 @@ class Login extends BaseHandler {
     // This function is used to get customer by cuid
     async checkIfCustomerExists(cuid) {
         let params = {
-            TableName: `customers-${process.env.STAGE}`,
+            TableName: `customer-${process.env.STAGE}`,
             KeyConditionExpression: "#cuid = :cuidValue",
             ExpressionAttributeNames: {
                 "#cuid": "cuid"
@@ -74,58 +73,137 @@ class Login extends BaseHandler {
         }
     }
 
-    //this function is for login
-    async initiateAuth(authData) {
-        let params = {
-            AuthFlow: 'USER_PASSWORD_AUTH',
-            /* required */
-            ClientId: authData.ClientId,
-            /* required */
-            AuthParameters: {
-                'USERNAME': authData.Username,
-                'PASSWORD': authData.Password
+    //this function get userpool id by cuid
+    async getResources(cuid) {
+        console.log("in get resources");
+        let name = `${cuid}-userpool`;
+        console.log(name);
+
+        var params = {
+            TableName: `customer-resources-${process.env.STAGE}`,
+            Key: {
+                "name": name
             }
         };
-        return await cognitoidentityserviceprovider.initiateAuth(params).promise();
+        let valRes = await dynamodb.get(params).promise();
+        console.log("response from get");
+        //console.log(valRes.Item.attributes.poolid);
+        if (valRes && 'Item' in valRes && valRes.Item && 'name' in valRes.Item && valRes.Item.name) {
+            return valRes.Item.attributes.poolid;
+        }
+        else {
+            return responseHandler.callbackRespondWithSimpleMessage(404, 'Userpool not created')
+        }
+    }
+    //this function returns state of customer
+    async getstate(event) {
+        var params = {
+            TableName: `${process.env.STAGE}-admin-customer-state`,
+            Key: {
+                "ip": event.requestContext.identity.sourceIp
+            }
+        };
+
+        let getres = await dynamodb.get(params).promise();
+        return getres;
+    }
+
+    //this function is for login
+    async initiateAuth(authData) {
+        try {
+            let params = {
+                AuthFlow: 'USER_PASSWORD_AUTH',
+                /* required */
+                ClientId: authData.ClientId,
+                /* required */
+                AuthParameters: {
+                    'USERNAME': authData.Username,
+                    'PASSWORD': authData.Password
+                }
+            };
+            let loginresp = await cognitoidentityserviceprovider.initiateAuth(params).promise();
+            this.log.debug("loginresp", loginresp);
+            console.log("loginresp", loginresp);
+            return true;
+        }
+        catch (err) {
+            console.log(err);
+            if (err && 'statusCode' in err && err.statusCode) {
+                if (err.statusCode === 400)
+                    return false;
+                else
+                    return responseHandler.callbackRespondWithSimpleMessage(500, err.message);
+            }
+        }
     }
 
     // This method handles customer id
     async handleCustomerId(event) {
+        console.log("in customer hadle");
         let body = JSON.parse(event.body);
 
         let customerExists = await this.checkIfCustomerExists(body.cuid);
-        if (customerExists) { }
-        // Add state in admin-customer-state
-        // {ip: <>, state: 'customerid', cuid: body.cuid}
-        //     const params = {
-        //         TableName: `${process.env.STAGE}-admin-customer-state`,
-        //           Item:{
-        //             "ip": ,
-        //             "state": title,
-        //             "cuid":
-        //             }
-        //         }
-        //     };
-        //     let valRes = await dynamodb.put(params).promise();
+        if (customerExists) {
+            //Add state in admin-customer-state
+            //{ip: <>, state: 'customerid', cuid: body.cuid}
+            let stateresp = this.getstate(event);
+            if (stateresp && 'cuid' in stateresp && stateresp.cuid && stateresp.cuid == body.cuid) {
+                return;
+            }
 
-        //     return responseHandler.callbackRespondWithJsonBody(200, { result: 'yes' });
-        // } else {
-        //     return responseHandler.callbackRespondWithJsonBody(200, { result: 'no' });
-        // }
+            const params = {
+                TableName: `${process.env.STAGE}-admin-customer-state`,
+                Item: {
+                    "ip": event.requestContext.identity.sourceIp,
+                    "state": 'customerid',
+                    "cuid": body.cuid
+                }
+            };
+            console.log(params);
+            let valRes = await dynamodb.put(params).promise();
+            return responseHandler.callbackRespondWithJsonBody(200, { result: 'yes' });
+        } else {
+            return responseHandler.callbackRespondWithJsonBody(200, { result: 'no' });
+        }
     }
 
     // This method handles username 
     async handleUsername(event) {
+
         let body = JSON.parse(event.body);
         // Get last state using ip address from admin-customer-state
         // If state not present - return no
+        let stateresp = await this.getstate(event);
+        console.log(stateresp);
         // If state is present and state value is 'customerid'
+        if (!(stateresp && 'state' in stateresp.Item && stateresp.Item.state && stateresp.Item.state === 'customerid')) {
+            //console.log(stateresp.state);
+            return responseHandler.callbackRespondWithJsonBody(200, { result: 'no' });
+        }
         // a. Get user pool details from customer resources table using cuid
+        let userpoolid = await this.getResources(stateresp.Item.cuid);
         // b. Check if username is present in userpool
+        let userpoolres = await this.checkUserExist(body.username, userpoolid);
         // c. if username is present then return yes
         // d. if username is not present then return no
-        // e. Update state // Add state in admin-customer-state
-        // {ip: <>, state: 'username', cuid: body.cuid, username: body.username}
+        if (userpoolres) {
+            // e. Update state // Add state in admin-customer-state
+            // {ip: <>, state: 'username', cuid: body.cuid, username: body.username}
+            const params = {
+                TableName: `${process.env.STAGE}-admin-customer-state`,
+                Item: {
+                    "ip": event.requestContext.identity.sourceIp,
+                    "state": 'username',
+                    "cuid": stateresp.Item.cuid,
+                    "username": body.username
+                }
+            };
+            console.log(params);
+            let valRes = await dynamodb.put(params).promise();
+            return responseHandler.callbackRespondWithJsonBody(200, { result: 'yes' });
+        } else {
+            return responseHandler.callbackRespondWithJsonBody(200, { result: 'no' });
+        }
     }
 
     // This method handles login 
@@ -133,12 +211,59 @@ class Login extends BaseHandler {
         let body = JSON.parse(event.body);
         // Get last state using ip address from admin-customer-state
         // If state not present - return no
+
+        let stateresp = await this.getstate(event);
+
         // If state is present and state value is 'username'
+        if (!(stateresp && 'state' in stateresp.Item && stateresp.Item.state && stateresp.Item.state === 'username')) {
+            //console.log(stateresp.state);
+            return responseHandler.callbackRespondWithJsonBody(200, { result: 'no' });
+        }
+
+        //get clientId from customer resources table
+        let cuid = stateresp.Item.cuid;
+        let name = `${cuid}-userpool`;
+        let paramsres = {
+            TableName: `customer-resources-${process.env.STAGE}`,
+            Key: {
+                "name": name
+            }
+        };
+        let respresource = await dynamodb.get(paramsres).promise();
+        console.log(respresource);
+        let clientId = respresource.Item.attributes.clientid;
+        console.log(clientId);
+
         // a.Check if login is correct
         // b. If login is correct -> return 'yes'
-        // c.  Update state // Add state in admin-customer-state
-        // {ip: <>, state: 'login', cuid: body.cuid, username: body.username, password: body.password}
-        // d. If login is not correct -> return 'no'
+        let authData = {
+            Username: stateresp.Item.username,
+            Password: body.password,
+            ClientId: clientId
+        };
+
+        let loginresp = await this.initiateAuth(authData);
+
+        if (loginresp) {
+            // c.  Update state // Add state in admin-customer-state
+            // {ip: <>, state: 'login', cuid: body.cuid, username: body.username, password: body.password}
+            // d. If login is not correct -> return 'no'
+            const params = {
+                TableName: `${process.env.STAGE}-admin-customer-state`,
+                Item: {
+                    "ip": event.requestContext.identity.sourceIp,
+                    "state": 'login',
+                    "cuid": stateresp.Item.cuid,
+                    "username": stateresp.Item.username,
+                    "password": "validationdone"
+                }
+            };
+            console.log(params);
+            let valRes = await dynamodb.put(params).promise();
+            return responseHandler.callbackRespondWithJsonBody(200, { result: 'yes' });
+        } else {
+            return responseHandler.callbackRespondWithJsonBody(200, { result: 'no' });
+        }
     }
 
     // This method handles mfa 
@@ -146,7 +271,15 @@ class Login extends BaseHandler {
         let body = JSON.parse(event.body);
         // Get last state using ip address from admin-customer-state
         // If state not present - return no
+
+        let stateresp = await this.getstate(event);
+ 
         // If state is present and state value is 'login'
+        if (!(stateresp && 'state' in stateresp.Item && stateresp.Item.state && stateresp.Item.state === 'login')) {
+            console.log(stateresp.state);
+            return responseHandler.callbackRespondWithJsonBody(200, { result: 'no' });
+        }
+
         // a.Check if login is correct with mfa
         // b. If login is correct -> return JWT (access token from cognito)
         // c. Delete state by ip
@@ -155,26 +288,25 @@ class Login extends BaseHandler {
 
     async process(event, context, callback) {
         try {
-            console.log("in process 2");
+
             let body = event.body ? JSON.parse(event.body) : event;
-            console.log(event.requestContext.identity.sourceIp);
+            //console.log(event.requestContext.identity.sourceIp);
 
-            // io.sockets.on('connection', function (socket) {
-            //     var address = socket.handshake.address;
-            //     console.log('New connection from ' + address.address + ':' + address.port);
-            // });
-
-            //this.log.debug("body----" + JSON.stringify(event.body));
+            this.log.debug("body----" + JSON.stringify(event.body));
             //await utils.validate(body, this.getValidationSchema());
 
+            // Scenario 1: Customer id is passed
+            if ('cuid' in body && body.cuid) {
+                return await this.handleCustomerId(event);
 
-            // // Scenario 1: Customer id is passed
-            // if ('cuid' in body && body.cuid) {
-            //     return await this.handleCustomerId(event);
-            // } else if ('username' in body && body.username) {
-            //     return await this.handleUsername(event);
-            // } else if ('password' in body && body.password) {
-            //     return await this.handleLogin(event);
+            } else if ('username' in body && body.username) {
+                console.log("in username handle");
+                return await this.handleUsername(event);
+
+            } else if ('password' in body && body.password) {
+
+                return await this.handleLogin(event);
+            }
             // } else if ('mfa' in body) {
             //     return await this.handleMFA(event);
             // }
